c26b66d416891cac61ff18347b40d267306202e5
Alan Kay <alanajkay@gmail.com>
2020-10-24 11:50:05 -0500

    Switched to nodenv from nvm

--- scripts/install-nvm-node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# Install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | zsh
-source ~/.zshrc
-
-# Install latest lts node and update to latest npm
-nvm install --lts
-npm install -g npm@latest
