8d61e7595de2b460bb0d5523f285f1450c8b39d9
Alan Kay <alanajkay@gmail.com>
2022-10-31 20:53:00 -0500

    switch to branch

--- scripts/install-asdf.sh
+++ scripts/install-asdf.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 # Install asdf and set to load
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
 sed -i 's/\(^plugins=([^)]*\)/\1 asdf/' ~/.zshrc
 source ~/.zshrc
