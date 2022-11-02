934c2ad535a2f34f2fa7a8f9ef288be2be9ae4b5
Alan Kay <alanajkay@gmail.com>
2022-11-01 20:22:20 -0500

    Update install-asdf.sh

--- scripts/install-asdf.sh
+++ scripts/install-asdf.sh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 
 # Install asdf and set to load
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
 sed -i 's/\(^plugins=([^)]*\)/\1 asdf/' ~/.zshrc
 source ~/.zshrc
+
+# Update asdf and plugins
+asdf update
+asdf plugin-update --all
